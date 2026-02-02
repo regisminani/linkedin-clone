df9d3e9855255e78344af356129a15b50a656538
Régis Minani <156519807+regisminani@users.noreply.github.com>
2026-02-02 13:14:55 +0200

    Update Firebase config with placeholder values
    Replaced sensitive Firebase configuration values with placeholders.

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDeXz4p4ZDAaZWquPMZWN-ORFJJfqj8nHA",
-  authDomain: "linkedin-clone-8aaf3.firebaseapp.com",
-  projectId: "linkedin-clone-8aaf3",
-  storageBucket: "linkedin-clone-8aaf3.appspot.com",
-  messagingSenderId: "1038699852360",
-  appId: "1:1038699852360:web:02ee98fc54a4a477a0de7d",
+  apiKey: "yourApiKey",
+  authDomain: "yourAuthDomain",
+  projectId: "yourProjectId",
+  storageBucket: "yourStorageBucket",
+  messagingSenderId: "yourMessagingSenderId",
+  appId: "yourAppId",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
